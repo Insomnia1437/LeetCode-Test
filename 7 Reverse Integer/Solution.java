4d8ba7492e9214a0c79aef41d9323832731d5515
Insomnia1437 <bssb2009@163.com>
2016-10-14 11:36:09 +0800

    handle overflow

---
+++ 7 Reverse Integer/Solution.java
@@ -0,0 +1,67 @@
+
+/**
+ Reverse digits of an integer.
+
+ Example1: x = 123, return 321
+ Example2: x = -123, return -321
+
+ click to show spoilers.
+
+ Have you thought about this?
+ Here are some good questions to ask before coding. Bonus points for you if you have already thought through this!
+
+ If the integer's last digit is 0, what should the output be? ie, cases such as 10, 100.
+
+ Did you notice that the reversed integer might overflow? Assume the input is a 32-bit integer, then the reverse of 1000000003 overflows. How should you handle such cases?
+
+ For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+ */
+public class Solution {
+    public int reverse(int x) {
+        String sx = String.valueOf(x);
+        int len = sx.length();
+        boolean isnegative = false;
+        String temp="";
+        for (int i = len-1; i >= 0; i--) {
+            if (sx.charAt(i)=='-'){
+                isnegative = true;
+                continue;
+            }
+            temp+=sx.charAt(i);
+        }
+        long res=0;
+        int l = temp.length()-1;
+        for (char c:temp.toCharArray()){
+            res+=Math.pow(10,l)*(c-48);
+            if (res>=Integer.MAX_VALUE)
+                return 0;
+            l--;
+        }
+        if (isnegative)
+            res*=-1;
+        return (int)res;
+
+    }
+    /*
+    public int reverse(int x) {
+	    int sign=x<0?-1:1;
+            x=Math.abs(x);
+	    long r=0;
+	    for ( ; x>0; x=x/10) {
+	    	r= r*10+x%10;
+	    	if (r>Integer.MAX_VALUE)
+	    	    return 0;
+	    }
+	    return (int)r*sign;
+	}
+     */
+
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.reverse(-1000000003));
+
+    }
+}
+
+
