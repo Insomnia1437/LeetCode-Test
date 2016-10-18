d5e3e1f08b50d7fce8df8cc1ac4de6148c2566eb
Insomnia1437 <bssb2009@163.com>
2016-10-18 16:11:03 +0800

    swap

--- 46 Permutations/Solution.java
+++ 46 Permutations/Solution.java
@@ -1,7 +1,18 @@
 import java.util.*;
 
 /**
- Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+ Given a collection of distinct numbers, return all possible permutations.
+
+ For example,
+ [1,2,3] have the following permutations:
+ [
+ [1,2,3],
+ [1,3,2],
+ [2,1,3],
+ [2,3,1],
+ [3,1,2],
+ [3,2,1]
+ ]
  */
 public class Solution {
     public List<List<Integer>> permute(int[] nums) {
