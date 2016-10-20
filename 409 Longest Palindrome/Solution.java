9384808e8818c6356944d1a86037a7f2daa7f899
Insomnia1437 <bssb2009@163.com>
2016-10-20 16:11:45 +0800

    hash

---
+++ 409 Longest Palindrome/Solution.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public int longestPalindrome(String s) {
+        int[] count = new int[Math.abs('A' - 'z')+1];
+        int ans = 0;
+        for(int i = 0; i< s.length(); i++) count[s.charAt(i) - 'A']++; 
+        for(int n: count) ans += n/2;
+        return Math.min(ans*2+1, s.length());
+    }
+}
