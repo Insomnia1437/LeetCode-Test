7c9486f7d41453806881a005a3673982f2de2b4a
Insomnia1437 <bssb2009@163.com>
2016-11-14 10:42:30 +0800

    DP

---
+++ 72 Edit Distance/Solution.java
@@ -0,0 +1,34 @@
+
+/**
+ Given two words word1 and word2, find the minimum number of steps required to convert word1 to word2. (each operation is counted as 1 step.)
+
+ You have the following 3 operations permitted on a word:
+
+ a) Insert a character
+ b) Delete a character
+ c) Replace a character
+ */
+public class Solution {
+    public int minDistance(String word1, String word2) {
+        int l1=word1.length(),l2=word2.length();
+        int dp[][] = new int[l1+1][l2+1];
+        for (int i = 0; i <= l1; i++) {
+            dp[i][0]=i;
+        }
+        for (int i = 0; i <= l2; i++) {
+            dp[0][i]=i;
+        }
+        for (int i = 1; i <= l1; i++) {
+            for (int j = 1; j <= l2; j++) {
+                dp[i][j] = Math.min((word1.charAt(i-1) == word2.charAt(j-1))?dp[i-1][j-1]:(dp[i-1][j-1]+1),Math.min(dp[i-1][j]+1,dp[i][j-1]+1));
+            }
+        }
+        return dp[l1][l2];
+    }
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.minDistance("cafe","coffee"));
+    }
+}
+
+
