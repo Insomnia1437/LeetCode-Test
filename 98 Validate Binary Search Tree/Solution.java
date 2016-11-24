08bbc9af9488ff911122f848ac95d187e1365bf3
Insomnia1437 <bssb2009@163.com>
2016-11-24 09:46:27 +0800

    DFS

---
+++ 98 Validate Binary Search Tree/Solution.java
@@ -0,0 +1,49 @@
+import java.util.Stack;
+
+/**
+ Given a binary tree, determine if it is a valid binary search tree (BST).
+
+ Assume a BST is defined as follows:
+
+ The left subtree of a node contains only nodes with keys less than the node's key.
+ The right subtree of a node contains only nodes with keys greater than the node's key.
+ Both the left and right subtrees must also be binary search trees.
+ Example 1:
+ 2
+ / \
+ 1   3
+ Binary tree [2,1,3], return true.
+ Example 2:
+ 1
+ / \
+ 2   3
+ Binary tree [1,2,3], return false.
+ */
+public class Solution {
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) return true;
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode pre = null;
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if(pre != null && root.val <= pre.val) return false;
+            pre = root;
+            root = root.right;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        TreeNode a = new TreeNode(2);
+        a.left = new TreeNode(1);
+        a.right = new TreeNode(3);
+        System.out.println(solution.isValidBST(a));
+
+    }
+}
+
+
