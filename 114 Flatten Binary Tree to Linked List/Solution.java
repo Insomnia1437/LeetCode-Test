d5db6a1055ef34fc1b631eb29ad952029c4c58ff
Insomnia1437 <bssb2009@163.com>
2017-01-04 16:15:15 +0800

    DFS

---
+++ 114 Flatten Binary Tree to Linked List/Solution.java
@@ -0,0 +1,47 @@
+
+/**
+ Given a binary tree, flatten it to a linked list in-place.
+
+ For example,
+ Given
+
+ 1
+ / \
+ 2   5
+ / \   \
+ 3   4   6
+ The flattened tree should look like:
+ 1
+ \
+ 2
+ \
+ 3
+ \
+ 4
+ \
+ 5
+ \
+ 6
+ **
+ */
+public class Solution {
+
+    TreeNode prev = null;
+    public void flatten(TreeNode root) {
+        if (root == null)
+            return;
+        flatten(root.right);
+        flatten(root.left);
+        root.right = prev;
+        root.left = null;
+        prev = root;
+    }
+
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+    }
+}
+
+
